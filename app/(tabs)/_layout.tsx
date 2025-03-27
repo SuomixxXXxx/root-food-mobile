7d5cc028b2381a6195688670b589ba1aa61b5035
Suomi228 <bixep345@gmail.com>
2025-03-27 08:19:28 +0300

    feat: tabs done

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,16 +6,51 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: "blue",
+        tabBarActiveTintColor: "#0396BF",
         tabBarInactiveTintColor: "gray",
+        tabBarStyle: {
+          backgroundColor: "white",
+          position: "absolute",
+          bottom: 20,
+          left: 0,
+          right: 0,
+          marginHorizontal: 20, 
+          borderRadius: 20,
+          height: 70,
+          elevation: 10,
+          shadowColor: "black",
+          shadowOffset: {
+            height: 0,
+            width: 0,
+          },
+          shadowOpacity: 0.1,
+          shadowRadius: 10,
+          
+        },
+        tabBarItemStyle: {
+          justifyContent: "center",
+          alignItems: "center",
+          height: 70,
+          paddingVertical: 8,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          marginTop: -4,
+          marginBottom: 4,
+        },
+        
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: "Главная",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home-outline" size={size} color={color} />
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={size}
+              color={color}
+            />
           ),
         }}
       />
@@ -23,8 +58,12 @@ export default function TabLayout() {
         name="catalog"
         options={{
           title: "Каталог",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="restaurant-outline" size={size} color={color} />
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? "restaurant" : "restaurant-outline"}
+              size={size}
+              color={color}
+            />
           ),
         }}
       />
@@ -32,8 +71,12 @@ export default function TabLayout() {
         name="cart"
         options={{
           title: "Корзина",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="cart-outline" size={size} color={color} />
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? "cart" : "cart-outline"}
+              size={size}
+              color={color}
+            />
           ),
         }}
       />
@@ -41,8 +84,12 @@ export default function TabLayout() {
         name="userInfo"
         options={{
           title: "Войти",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="person-circle-outline" size={size} color={color} />
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? "person-circle" : "person-circle-outline"}
+              size={size}
+              color={color}
+            />
           ),
         }}
       />
